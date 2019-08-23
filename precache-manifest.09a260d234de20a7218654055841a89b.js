3f30cc14b51bf4bdf4c1635ededb28c65f42280f
vjenacka <edins613@gmail.com>
2019-08-23 13:48:08 +0200

    Updates

---
+++ precache-manifest.09a260d234de20a7218654055841a89b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc07a1b9966b36a12fbde8901b80fbc",
+    "url": "/FCC-Drum-Machine/index.html"
+  },
+  {
+    "revision": "5aa5a8051580404b1f78",
+    "url": "/FCC-Drum-Machine/static/css/main.661760e2.chunk.css"
+  },
+  {
+    "revision": "46d799c001f157aa8c3a",
+    "url": "/FCC-Drum-Machine/static/js/2.6a98ba04.chunk.js"
+  },
+  {
+    "revision": "5aa5a8051580404b1f78",
+    "url": "/FCC-Drum-Machine/static/js/main.0bb3e89b.chunk.js"
+  },
+  {
+    "revision": "1cece5098f1f3bde1738",
+    "url": "/FCC-Drum-Machine/static/js/runtime~main.874bd421.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/FCC-Drum-Machine/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/FCC-Drum-Machine/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/FCC-Drum-Machine/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/FCC-Drum-Machine/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/FCC-Drum-Machine/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/FCC-Drum-Machine/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/FCC-Drum-Machine/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/FCC-Drum-Machine/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/FCC-Drum-Machine/static/media/tom.572ed39d.wav"
+  }
+]);
